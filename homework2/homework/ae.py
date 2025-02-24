970e1568d396ec6bd825229f3c9149c58d4fde1e
Xin <edi.wangxin@gmail.com>
2025-02-23 19:52:47 -0500

    Update ae.py

--- homework2/homework/ae.py
+++ homework2/homework/ae.py
@@ -101,8 +101,10 @@ class PatchAutoEncoder(torch.nn.Module, PatchAutoEncoderBase):
     class PatchEncoder(torch.nn.Module):
         def __init__(self, patch_size: int, latent_dim: int, bottleneck: int):
             super().__init__()
-            self.conv1 = torch.nn.Conv2d(3, bottleneck*2, kernel_size=patch_size, stride=patch_size)
-            self.conv2 = torch.nn.Conv2d(bottleneck*2, latent_dim, kernel_size=3, stride=1, padding=1)
+            self.conv1 = torch.nn.Conv2d(3, bottleneck, kernel_size=3, stride=2, padding=1)
+            self.conv2 = torch.nn.Conv2d(bottleneck, latent_dim, kernel_size=patch_size, stride=patch_size)
+            # self.conv1 = torch.nn.Conv2d(3, bottleneck*2, kernel_size=patch_size, stride=patch_size)
+            # self.conv2 = torch.nn.Conv2d(bottleneck*2, latent_dim, kernel_size=3, stride=1, padding=1)
 
             # self.patchify = PatchifyLinear(patch_size, latent_dim)
 
@@ -124,8 +126,10 @@ class PatchAutoEncoder(torch.nn.Module, PatchAutoEncoderBase):
     class PatchDecoder(torch.nn.Module):
         def __init__(self, patch_size: int, latent_dim: int, bottleneck: int):
             super().__init__()
-            self.conv1 = torch.nn.ConvTranspose2d(latent_dim, bottleneck*2, kernel_size=3, stride=1, padding=1)
-            self.conv2 = torch.nn.ConvTranspose2d(bottleneck*2, 3, kernel_size=patch_size, stride=patch_size)
+            self.conv1 = torch.nn.ConvTranspose2d(latent_dim, bottleneck, kernel_size=3, stride=2, padding=1)
+            self.conv2 = torch.nn.ConvTranspose2d(bottleneck, 3, kernel_size=patch_size, stride=patch_size)
+            # self.conv1 = torch.nn.ConvTranspose2d(latent_dim, bottleneck*2, kernel_size=3, stride=1, padding=1)
+            # self.conv2 = torch.nn.ConvTranspose2d(bottleneck*2, 3, kernel_size=patch_size, stride=patch_size)
 
             # self.unpatchify = UnpatchifyLinear(patch_size, latent_dim)
 
