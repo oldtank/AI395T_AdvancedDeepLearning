89a6197f68afea4990a125cbdc8f5931ec4e5691
Xin <edi.wangxin@gmail.com>
2025-02-24 14:14:05 -0500

    Update ae.py

--- homework2/homework/ae.py
+++ homework2/homework/ae.py
@@ -104,9 +104,9 @@ class PatchAutoEncoder(torch.nn.Module, PatchAutoEncoderBase):
             # self.conv1 = torch.nn.Conv2d(3, bottleneck, kernel_size=3, stride=2, padding=1)
             # self.conv2 = torch.nn.Conv2d(bottleneck, latent_dim, kernel_size=patch_size, stride=patch_size)
 
-            self.conv1 = nn.Conv2d(3, latent_dim, kernel_size=patch_size, stride=patch_size)
-            self.conv2 = nn.Conv2d(latent_dim, bottleneck * 2, kernel_size=3, stride=2, padding=1)
-            self.conv3 = nn.Conv2d(bottleneck * 2, bottleneck, kernel_size=3, stride=2, padding=1)
+            self.conv1 = torch.nn.Conv2d(3, latent_dim, kernel_size=patch_size, stride=patch_size)
+            self.conv2 = torch.nn.Conv2d(latent_dim, bottleneck * 2, kernel_size=3, stride=2, padding=1)
+            self.conv3 = torch.nn.Conv2d(bottleneck * 2, bottleneck, kernel_size=3, stride=2, padding=1)
 
             # self.patchify = PatchifyLinear(patch_size, latent_dim)
 
@@ -126,9 +126,9 @@ class PatchAutoEncoder(torch.nn.Module, PatchAutoEncoderBase):
             # self.conv1 = torch.nn.ConvTranspose2d(latent_dim, bottleneck, kernel_size=patch_size, stride=patch_size)
             # self.conv2 = torch.nn.ConvTranspose2d(bottleneck, 3, kernel_size=3, stride=2, padding=1, output_padding=1)
 
-            self.deconv1 = nn.ConvTranspose2d(bottleneck, bottleneck * 2, kernel_size=4, stride=2, padding=1)
-            self.deconv2 = nn.ConvTranspose2d(bottleneck * 2, latent_dim, kernel_size=4, stride=2, padding=1)
-            self.deconv3 = nn.ConvTranspose2d(latent_dim, 3, kernel_size=patch_size, stride=patch_size)
+            self.deconv1 = torch.nn.ConvTranspose2d(bottleneck, bottleneck * 2, kernel_size=4, stride=2, padding=1)
+            self.deconv2 = torch.nn.ConvTranspose2d(bottleneck * 2, latent_dim, kernel_size=4, stride=2, padding=1)
+            self.deconv3 = torch.nn.ConvTranspose2d(latent_dim, 3, kernel_size=patch_size, stride=patch_size)
 
             # self.unpatchify = UnpatchifyLinear(patch_size, latent_dim)
 
