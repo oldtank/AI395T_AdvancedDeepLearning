adff32e67095dd8e4ab649135ac93e0950c20f1f
Xin <edi.wangxin@gmail.com>
2025-02-27 16:27:40 -0500

    Update bsq.py

--- homework2/homework/bsq.py
+++ homework2/homework/bsq.py
@@ -101,7 +101,7 @@ class BSQPatchAutoEncoder(PatchAutoEncoder, Tokenizer):
           Changing the patch-size of codebook-size will complicate later parts of the assignment.
     """
 
-    def __init__(self, patch_size: int = 4, latent_dim: int = 128, codebook_bits: int = 15):
+    def __init__(self, patch_size: int = 5, latent_dim: int = 128, codebook_bits: int = 15):
         super().__init__(patch_size=patch_size, latent_dim=latent_dim, bottleneck=128)
         self.codebook_bits = codebook_bits
         self.bsq = BSQ(codebook_bits, latent_dim)
@@ -137,7 +137,6 @@ class BSQPatchAutoEncoder(PatchAutoEncoder, Tokenizer):
         # reconstructed_image = self.decode_index(self.encode_index(x))
         # x = self.encode(x)
         encoded_indices = self.encode_index(x)
-        print("bottleneck size: ", encoded_indices.shape[1] * encoded_indices.shape[2])
         
         cnt = torch.bincount(encoded_indices.flatten(), minlength=2 ** self.codebook_bits)
         reconstructed = self.decode_index(encoded_indices)
