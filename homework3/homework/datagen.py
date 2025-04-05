c073f70d0f61c6b731bb3a9091e9d836bfdc1572
Xin <edi.wangxin@gmail.com>
2025-04-04 23:29:59 -0400

    Update datagen.py

--- homework3/homework/datagen.py
+++ homework3/homework/datagen.py
@@ -1,4 +1,15 @@
+from .cot import CoTModel
+import torch
+
+checkpoint = "HuggingFaceTB/SmolLM2-360M-Instruct"
+device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
 def generate_dataset(output_json: str, oversample: int = 10, temperature: float = 0.6):
+    # questions = [["Can you change 2 hour to its equivalent in min?", 120.0], ["Express 4 centuries as a quantity of week.", 20870.982787499997]]
+    questions = ["Can you change 2 hour to its equivalent in min?","Express 4 centuries as a quantity of week." ]
+    model = CoTModel(checkpoint=checkpoint)
+
+    prompts = [model.format_prompt(q) for q in questions]
+    generations = model.batched_generate(prompts, num_return_sequences=oversample, temperature=temperature)
     raise NotImplementedError()
 
 
